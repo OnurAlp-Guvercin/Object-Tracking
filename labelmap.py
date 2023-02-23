aa767c34ba6b1712356abc28fd44b780cd67de0d
OnurAlp-Guvercin <117316200+OnurAlp-Guvercin@users.noreply.github.com>
2023-02-23 09:13:47 +0300

    Delete labelmap.py

--- labelmap.py
+++
@@ -1,9 +0,0 @@
-labels = [{'name':'bolt', 'id':1}, {'name':'nut', 'id':2}]
-label_map = {'item': labels}
-
-with open('label_map.pbtxt', 'w') as f:
-    for label in labels:
-        f.write('item {\n')
-        f.write('  id: {}\n'.format(label['id']))
-        f.write('  name: "{}"\n'.format(label['name']))
-        f.write('}\n')
